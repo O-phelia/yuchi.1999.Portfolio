0c8d7cb0b40e3635103049b7a550e284ef25ac61
O-phelia <pattylo0813@gmail.com>
2023-05-29 18:57:35 +0800

    動畫OK

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+let Back = document.getElementById('mainBack');
+let Cloud = document.getElementById('mainCloud');
+let Right = document.getElementById('mainFont-Right');
+let Left = document.getElementById('mainFont-Left');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    Cloud.style.left = value*0.25+'px';
+    Right.style.left = value*-0.7+'px';
+    Left.style.left = value*0.63+'px';
+    Back.style.top = value*-0.1+'px';
+})
