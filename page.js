412c9746af6016035274e1cb1d4a54f0ddb23c3a
O-phelia <pattylo0813@gmail.com>
2023-06-01 12:30:45 +0800

    all-nav

--- page.js
+++ page.js
@@ -3,12 +3,12 @@ let bugar_close = document.getElementById('bugar');
 bugar_close.addEventListener('click',function(){
     let currentImageSrc = bugar.getAttribute('src');
 
-  // 检查当前图片是否是bugar.png
-  if (currentImageSrc === './img/Top-img/PNG/bugar.png') {
-    // 将src属性值设置为另一张图片的路径
+    // 檢查是否是bugar.png
+    if (currentImageSrc === './img/Top-img/PNG/bugar.png') {
+    // 如是 將設為另一圖片
     bugar.setAttribute('src', './img/Top-img/PNG/icon_close.png');
-  } else {
-    // 将src属性值设置回原始的bugar.png路径
+    } else {
+    // 返回原本圖片
     bugar.setAttribute('src', './img/Top-img/PNG/bugar.png');
 }
 })
