91cd94770e10ce75365c65db886564908a758669
O-phelia <pattylo0813@gmail.com>
2023-06-10 22:15:23 +0800

    animate set

--- page.js
+++ page.js
@@ -2,6 +2,7 @@ let bugar_close = document.getElementById('bugar');
 let point = document.getElementById("point");
 let effects = document.querySelectorAll(".effect");
 let txts = document.querySelectorAll(".Txt");
+let box = document.querySelectorAll(".box");
 
 bugar_close.addEventListener('click',function(){
     let currentImageSrc = bugar.getAttribute('src');
@@ -16,38 +17,34 @@ bugar_close.addEventListener('click',function(){
 }
 })
 
+
+
 effects.forEach(function(effect) {
 
+  var timeoutId; // 用于存储延迟操作的超时ID
+
   effect.addEventListener("mouseover", function() {
+    clearTimeout(timeoutId); // 如果存在延迟操作，则在鼠标移入时清除它
     txts.forEach(function(txt) {
       txt.style.color = "rgb(43, 42, 40)";
     });
   });
 
   effect.addEventListener("mouseout", function() {
-    txts.forEach(function(txt) {
-      txt.style.color = "rgb(255, 0, 162)";
-    });
+    clearTimeout(timeoutId); // 如果存在延迟操作，则在鼠标移出时清除它
+
+    timeoutId = setTimeout(function() {
+      txts.forEach(function(txt) {
+        txt.style.color = "rgb(255, 0, 162)";
+      });
+    }, 300); // 设置延迟时间（以毫秒为单位）
   });
-  
+
 });
 
 
-// effect.addEventListener("mouseover", function() {
-//   Txt.style.color = "rgb(43, 42, 40)" ;
-// });
-
-// effect.addEventListener("mouseout", function() {
-//   Txt.style.color = "rgb(255, 0, 162)";
-// });
 
 
 
-// onetime.addEventListener("mouseenter", function() {
-//   point.style.display = "block";
-// });
-
-// onetime.addEventListener("mouseleave", function() {
-//   point.style.display = "none";
-// });
+// 偵測滾動事件
 
