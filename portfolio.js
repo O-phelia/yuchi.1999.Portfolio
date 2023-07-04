84274d38c678bd3051cf14033188a436479edd52
O-phelia <pattylo0813@gmail.com>
2023-07-04 14:23:25 +0800

    100%

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,31 @@ window.addEventListener('scroll', function(){
 })
 
 
+
+
+// 預設滾輪原始樣式
+var originalOverflowStyle = document.body.style.overflow;
+
+// 移除滾輪功能
+document.body.style.overflow = 'hidden';
+
+// loading
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        var status = document.getElementById('status');
+        var preloader = document.getElementById('preloader');
+
+        if (status) {
+            status.style.transition = 'opacity 0.5s';
+            status.style.opacity = '0';
+        }
+
+        if (preloader) {
+            preloader.style.transition = 'opacity 0.5s';
+            preloader.style.opacity = '0';
+
+            // 恢復滾輪原始樣式
+            document.body.style.overflow = originalOverflowStyle;
+        }
+    }, 2300); // 延遲 2300 毫秒
+});
